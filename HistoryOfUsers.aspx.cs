fd5d4f3db54558b0f3ed3449ccaff1ca1541d6da
Udesh696 <udesh696@gmail.com>
2018-06-27 04:03:50 +0530

    Final

---
+++ HistoryOfUsers.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace STMSM
+{
+    public partial class HistoryOfUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HistoryOFData();
+        }
+
+        public void HistoryOFData()
+        {
+            HistoryD gm = new HistoryD();
+            DataTable dt = new DataTable();
+            dt = gm.MarketDetails(Session["UserID"].ToString());
+            RptHistory.DataSource = dt.DefaultView;
+            RptHistory.DataBind();
+        
+        }
+    }
+}
